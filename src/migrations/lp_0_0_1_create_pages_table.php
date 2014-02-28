dfadfd3f71769e397e3712b21236fc3f11653d17
JeroenG <jeroengjeroeng@gmail.com>
2014-02-28 18:39:00 +0100

    New file: pages table migration

---
+++ src/migrations/lp_0_0_1_create_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('pages', function(Blueprint $table)
+		{
+			$table->increments('page_id');
+			$table->string('page_title');
+			$table->text('page_content');
+			$table->string('page_slug')->unique();
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('pages');
+	}
+
+}
