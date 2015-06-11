df833c56177879aee882a94b6dd594b7b28507f2
JeroenG <jeroengjeroeng@gmail.com>
2015-06-11 11:17:39 +0200

    Updated tests, removed Travis

--- tests/LaravelPagesTest.php
+++ tests/LaravelPagesTest.php
@@ -1,10 +1,6 @@
 <?php
 
 use JeroenG\LaravelPages\LaravelPages;
-use Illuminate\Foundation\Testing\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 /**
  * This is for testing the package
@@ -22,29 +18,6 @@ class LaravelPagesTest extends TestCase
      * @var object
      */
     protected $pages;
-
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../vendor/laravel/laravel/bootstrap/app.php';
-
-        $app->register('JeroenG\LaravelPages\LaravelPagesServiceProvider');
-
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
     
     /**
      * Setup DB before each test.
