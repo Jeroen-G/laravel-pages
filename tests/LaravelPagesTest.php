efd5c486b9a9d033ca43f4ecd0147ae99ee0516d
JeroenG <jeroengjeroeng@gmail.com>
2015-06-11 10:51:00 +0200

    Updated tests

--- tests/LaravelPagesTest.php
+++ tests/LaravelPagesTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use JeroenG\LaravelPages\LaravelPages;
+use Illuminate\Foundation\Testing\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -22,6 +23,29 @@ class LaravelPagesTest extends TestCase
      */
     protected $pages;
 
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../vendor/laravel/laravel/bootstrap/app.php';
+
+        $app->register('JeroenG\LaravelPages\LaravelPagesServiceProvider');
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+    
     /**
      * Setup DB before each test.
      *
